7a7a201c67947cfc64ce98154cf422dbef740440
Sara Tisell <Sara.Tisell@hotmail.com>
2024-03-13 08:36:09 +0000

    Add meta keyword & descript to index page

--- by_noveline/settings.py
+++ by_noveline/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-saratisell-bynoveline-9krmh26dqga.ws-eu108.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-saratisell-bynoveline-hg8wucnfwsa.ws-eu109.gitpod.io','.herokuapp.com']
 
 
 # Application definition
@@ -124,7 +124,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.8000-saratisell-bynoveline-9krmh26dqga.ws-eu108.gitpod.io",
+    "https://*.8000-saratisell-bynoveline-hg8wucnfwsa.ws-eu109.gitpod.io",
     "https://*.herokuapp.com"
 ]
 
