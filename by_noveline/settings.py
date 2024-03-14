c6f76db251a8cc9c5e5a47f104105722a813cbdb
Sara Tisell <Sara.Tisell@hotmail.com>
2024-03-14 10:26:46 +0000

    Add base templ to allauth pages & add styling

--- by_noveline/settings.py
+++ by_noveline/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-saratisell-bynoveline-rqomo0rirdc.ws-eu110.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-saratisell-bynoveline-qrq2r8psvok.ws-eu110.gitpod.io','.herokuapp.com']
 
 
 # Application definition
@@ -124,7 +124,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.8000-saratisell-bynoveline-rqomo0rirdc.ws-eu110.gitpod.io",
+    "https://*.8000-saratisell-bynoveline-qrq2r8psvok.ws-eu110.gitpod.io",
     "https://*.herokuapp.com"
 ]
 
