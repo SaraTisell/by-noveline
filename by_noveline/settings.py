44e59c6dc4b66fc5e03f798b23799e4a9163f087
Sara Tisell <Sara.Tisell@hotmail.com>
2024-03-18 10:54:46 +0000

    Update categories, add json for products & add media

--- by_noveline/settings.py
+++ by_noveline/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-saratisell-bynoveline-julxug4ua8g.ws-eu110.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-saratisell-bynoveline-ix532lvkq3i.ws-eu110.gitpod.io','.herokuapp.com']
 
 
 # Application definition
@@ -125,7 +125,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.8000-saratisell-bynoveline-julxug4ua8g.ws-eu110.gitpod.io",
+    "https://*.8000-saratisell-bynoveline-ix532lvkq3i.ws-eu110.gitpod.io",
     "https://*.herokuapp.com"
 ]
 
