34e71b44c6cdbe1b4ab708bfedf4c09ce5f22cfa
Sara Tisell <Sara.Tisell@hotmail.com>
2024-03-19 09:15:02 +0000

    Customize admin interface

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from .models import Category, Product
 
-admin.site.register(Category)
-admin.site.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+    ordering = ('name',)
+    
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'sku',
+        'price',
+        'category',
+        'image',
+    )
+
+    ordering = ('category',)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
