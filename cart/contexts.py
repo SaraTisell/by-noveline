459ad9ea68a0a97673dda1a567e247d522c018b8
Sara Tisell <Sara.Tisell@hotmail.com>
2024-03-22 12:26:01 +0000

    Update cart template

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,11 +1,25 @@
+from decimal import Decimal
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def cart_contents(request):
 
     cart_items = []
     product_count = 0
     subtotal = 0
-    tax_rate = 0.10
+    tax_rate = Decimal('0.10')
     shipping_cost = 9
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        subtotal += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     tax = subtotal * tax_rate
 
