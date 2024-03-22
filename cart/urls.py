3dd99100a98ef988e2c216c0c764636ce2d66a6e
Sara Tisell <Sara.Tisell@hotmail.com>
2024-03-22 10:14:26 +0000

    Add add to cart functionality

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import ViewShoppingCart
+from . import views
 
 urlpatterns = [
     path('', ViewShoppingCart.as_view(), name='cart'),
+    path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
     
 ]
