c60612acbca5afff1d6ff15cf1355abaa7d22717
Sara Tisell <Sara.Tisell@hotmail.com>
2024-03-24 20:13:04 +0000

    Update add to cart to include size

--- cart/contexts.py
+++ cart/contexts.py
@@ -11,18 +11,23 @@ def cart_contents(request):
     shipping_cost = 9
     cart = request.session.get('cart', {})
 
-    for item_id, quantity in cart.items():
-        product = get_object_or_404(Product, pk=item_id)
-        subtotal += quantity * product.price
-        product_count += quantity
-        cart_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in cart.items():
+        if isinstance(item_data, dict):
+            quantity = item_data.get('quantity', 1)
+            size = item_data.get('size')
+            size2 = item_data.get('size2')
+            product = get_object_or_404(Product, pk=item_id)
+            subtotal += quantity * product.price
+            product_count += quantity
+            cart_items.append({
+                'item_id': item_id,
+                'quantity': quantity,
+                'product': product,
+                'size': size,
+                'size2': size2,
+            })   
 
     tax = subtotal * tax_rate
-
     grand_total = shipping_cost + subtotal + tax
 
 
--- cart/views.py
+++ cart/views.py
@@ -10,12 +10,29 @@ def add_to_cart(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = request.POST.get('selected_size')
+    size2 = None
+    if 'selected_size_ring_set' in request.POST:
+        size2 = request.POST['selected_size_ring_set']
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id]+= quantity
+
+    if item_id not in cart:
+        cart[item_id] = {}
+
+        if size in cart[item_id]:
+            cart[item_id][size] += quantity
+        else:
+            cart[item_id][size] = quantity
     else:
-        cart[item_id] = quantity
+        cart[item_id] = {size: quantity}
+    
+        if size2:
+            if item_id in cart:
+                cart[item_id]['size2'] = size2
+            else:
+                cart[item_id] = {size: quantity, 'size2': size2}
+
 
     request.session['cart'] = cart
     return redirect(redirect_url)
