b64d33791526e70ebdb33469c2dc4e53d19a786b
Sara Tisell <Sara.Tisell@hotmail.com>
2024-04-01 15:00:38 +0000

    Add message in base.html & add messages to cart views

--- cart/views.py
+++ cart/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect, reverse
 from django.views.generic import TemplateView
+from django.contrib import messages
 
 
 class ViewShoppingCart(TemplateView):
     """ View to render cart """
 
     template_name = 'cart/cart.html'
-
 
 def add_to_cart(request, item_id):
     """ Function to add item to cart """
@@ -28,33 +28,35 @@ def add_to_cart(request, item_id):
         }
     
     cart[cart_key]['quantity'] += quantity
+    messages.success(request, "Added product to cart")
 
     request.session['cart'] = cart
     return redirect(redirect_url)
 
-
-
 def adjust_cart(request, cart_key):
     """ Function to adjust quantity for a item in the cart """
+
     cart = request.session.get('cart', {})
     quantity = int(request.POST.get('quantity'))
 
     if cart_key in cart:
         if quantity > 0:
             cart[cart_key]['quantity'] = quantity
+            messages.success(request, "Updated product quantity")
         else:
             del cart[cart_key]
+            messages.success(request, "Removed product from cart")
 
     request.session['cart'] = cart
-
     return redirect(reverse('cart'))
 
 def remove_from_cart(request, cart_key):
     """ Function to remove a item from the cart """
+
     cart = request.session.get('cart', {})
 
     if cart_key in cart:
         del cart[cart_key]
-
+        
     request.session['cart'] = cart
     return redirect(reverse('cart'))
