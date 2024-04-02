ac9667edda90a789e35eebc40f8ca0b909ec3155
Sara Tisell <Sara.Tisell@hotmail.com>
2024-04-02 16:01:23 +0000

    Update & migrate order and orderlineitem models

--- checkout/models.py
+++ checkout/models.py
@@ -2,6 +2,7 @@ import uuid
 from django.db import models
 from django.conf import settings
 from django.db.models import Sum
+from decimal import Decimal
 from products.models import Product
 
 class Order(models.Model):
@@ -17,15 +18,25 @@ class Order(models.Model):
     county = models.CharField(max_length=80, null=True, blank=True)
     order_date = models.DateTimeField(auto_now_add=True)
     sub_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    shipping_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    tax = models.DecimalField(max_digits=8, decimal_places=2, null=False, default=0)
+    shipping_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=9)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
     def _generate_order_number(self):
 
+        """ Using UUID to generate unique order number """
         return uuid.uuid4().hex.upper()
 
-    def save(self, *args, **kwargs):
+    def update_total(self):
+        """ Update grand total based on line items """
+        self.sub_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.shipping_cost = Decimal(settings.SHIPPING_COST)
+        self.tax = self.sub_total * Decimal(settings.TAX_RATE)
+        self.grand_total = self.sub_total + self.tax + self.shipping_cost
+        self.save()
 
+    def save(self, *args, **kwargs):
+        """ Override default save method to set order number """
         if not self.order_number:
             self.order_number =self._generate_order_number()
         super().save(*args, **kwargs)
@@ -39,5 +50,13 @@ class OrderLineItem(models.Model):
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=10)
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    lineitem_total = models.DecimalField(max_digits=8, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """ Override dafault save method to set lineitem total and update sub total """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
 
