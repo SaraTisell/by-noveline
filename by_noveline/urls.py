6cb277beb5b88b814c0e180d9cf6f2fbb5727e3f
Sara Tisell <Sara.Tisell@hotmail.com>
2024-04-03 10:21:47 +0000

    Update form placeholders, create template, view, urls & css folder/file

--- by_noveline/urls.py
+++ by_noveline/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("cart/", include("cart.urls")),
+    path("checkout/", include("checkout.urls")),
     path('products/', include('products.urls')) ,
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- checkout/forms.py
+++ checkout/forms.py
@@ -25,9 +25,8 @@ class OrderForm(forms.ModelForm):
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
+            placeholder = placeholders[field]
             if self.fields[field].required:
                 placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
-                self.fields[field].label = False
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
