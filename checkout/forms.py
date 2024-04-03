0e76b95bf7f209d46806b4c9e19cc89f67f95c67
Sara Tisell <Sara.Tisell@hotmail.com>
2024-04-03 15:53:18 +0000

    Add class to form. Add stripe elements & style

--- checkout/forms.py
+++ checkout/forms.py
@@ -29,4 +29,5 @@ class OrderForm(forms.ModelForm):
             if self.fields[field].required:
                 placeholder = f'{placeholders[field]} *'
             self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
--- checkout/views.py
+++ checkout/views.py
@@ -12,7 +12,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51P1QzoDmTB5DF9hE1quFMg9XAb65GpfbSHjuKJtJXPhzLARXJLxR8mq6D692jliqAUmLQkbQlvDZwa2rVIJUd1lh00KcOyfq6d',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
