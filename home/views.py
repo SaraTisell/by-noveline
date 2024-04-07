9984f40ca2e7b12e394ab72f4b0af63d6c861950
Sara Tisell <Sara.Tisell@hotmail.com>
2024-04-07 16:29:39 +0000

    Add privacy policy, view template and urls

--- home/views.py
+++ home/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     """ View to return index page """
-    
-    return render (request, 'home/index.html')
+    return render(request, 'home/index.html')
+
+def privacy_policy(request):
+    """ View to render privacy policy """
+    return render(request, 'home/privacy_policy.html')
