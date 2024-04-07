0e7ac42920aaa8187119c9de8d3dc530220b3957
Sara Tisell <Sara.Tisell@hotmail.com>
2024-04-07 11:58:40 +0000

    Add order and order detail view, template & urls for superuser

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UserProfileView
+from .views import UserProfileView, AdminOrdersView, AdminOrderDetailView
 
 urlpatterns = [
     path('', UserProfileView.as_view(), name='profile'),
+    path('orders/', AdminOrdersView.as_view(), name='orders'),
+    path('order/<int:pk>/', AdminOrderDetailView.as_view(), name='order_detail'),
     
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,12 @@
-from django.views.generic import UpdateView
+from django.views.generic import UpdateView, ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.shortcuts import render
 from .models import UserProfile
 from .forms import UserDeliveryInfo
+from checkout.forms import OrderForm
 
 from checkout.models import Order
 
@@ -29,3 +31,24 @@ class UserProfileView(UpdateView):
         context['orders'] = orders
         print(orders)
         return context
+
+class AdminOrdersView(UserPassesTestMixin, ListView):
+    model = Order
+    template_name = 'profiles/orders.html'
+    get_success_url = reverse_lazy('orders')
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Order.objects.all()
+
+    def test_func(self):
+        if self.request.user.is_superuser:
+            return True
+
+class AdminOrderDetailView(UserPassesTestMixin, DetailView):
+    model = Order
+    template_name = 'profiles/order_detail.html'
+
+    def test_func(self):
+        if self.request.user.is_superuser:
+            return True
