489406580a6d8f30a8ba6e5740f27cd15f8e77a9
Sara Tisell <Sara.Tisell@hotmail.com>
2024-04-08 18:40:15 +0000

    Create wishlist app & add models

--- by_noveline/settings.py
+++ by_noveline/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-saratisell-bynoveline-a3jj9vn30tv.ws-eu110.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-saratisell-bynoveline-j3onvnui56z.ws-eu110.gitpod.io','.herokuapp.com']
 
 
 # Application definition
@@ -137,7 +137,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.8000-saratisell-bynoveline-a3jj9vn30tv.ws-eu110.gitpod.io",
+    "https://*.8000-saratisell-bynoveline-j3onvnui56z.ws-eu110.gitpod.io",
     "https://*.herokuapp.com"
 ]
 
---
+++ wishlist/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
+
+class WishList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return f"{self.user.username}'s Wishlist"
+
+class WishListItem(models.Model):
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    wishlist = models.ForeignKey(WishList, null=False, blank=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product.name
