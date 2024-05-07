3b08030504a163663d4eeae877ec2775d2346dca
Sara Tisell <Sara.Tisell@hotmail.com>
2024-05-07 08:27:34 +0000

    Create view to render latest products on homepage

--- home/views.py
+++ home/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from products.models import Product
 
 
 def index(request):
-    """ View to return index page """
-    return render(request, 'home/index.html')
+    """ View to return index as home page
+        Include latest products ordered by id
+    """
+    latest_products = Product.objects.order_by('-id')[:4]
+
+    return render(request, 'home/index.html', {'latest_products': latest_products})
 
 def privacy_policy(request):
     """ View to render privacy policy """
