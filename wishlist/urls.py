3e054bd4626127ad22cff6b2b562efb4ad1a5125
Sara Tisell <Sara.Tisell@hotmail.com>
2024-05-08 11:13:31 +0000

    Fix code to meet pep8 requirements

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', ViewWishList.as_view(), name='wishlist'),
     path('add-to-wishlist/<int:product_id>',
          views.add_to_wishlist, name='add_to_wishlist'),
-    path('delete-from-wishlist/<int:product_id>', views.delete_from_wishlist, name='delete_from_wishlist'),
-
+    path('delete-from-wishlist/<int:product_id>', views.delete_from_wishlist,
+         name='delete_from_wishlist'),
 ]
