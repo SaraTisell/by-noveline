9dc0fcb2d4890328dd3223477d8a8ae137b30862
Sara Tisell <Sara.Tisell@hotmail.com>
2024-05-08 11:21:42 +0000

    Fix code to meet pep8 requirements

--- wishlist/models.py
+++ wishlist/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from products.models import Product
 
+
 class WishList(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
@@ -9,9 +10,12 @@ class WishList(models.Model):
     def __str__(self):
         return f"{self.user.username}'s Wishlist"
 
+
 class WishListItem(models.Model):
-    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    wishlist = models.ForeignKey(WishList, null=False, blank=False, on_delete=models.CASCADE)
+    product = models.ForeignKey(
+            Product, null=False, blank=False, on_delete=models.CASCADE)
+    wishlist = models.ForeignKey(
+            WishList, null=False, blank=False, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.product.name
