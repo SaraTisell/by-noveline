4ad41553fa894ad49b14863ad3a6984e600f7ac1
Sara Tisell <Sara.Tisell@hotmail.com>
2024-05-08 11:27:57 +0000

    Fix code to meet pep8 requirements

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import UserProfile
 
+
 class UserDeliveryInfo(forms.ModelForm):
     class Meta:
         model = UserProfile
--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import UserProfileView, AdminOrdersView, AdminOrderDetailView, AdminDeleteOrder
+from .views import (
+    UserProfileView, AdminOrdersView, AdminOrderDetailView, AdminDeleteOrder
+    )
 
 urlpatterns = [
     path('', UserProfileView.as_view(), name='profile'),
     path('orders/', AdminOrdersView.as_view(), name='orders'),
-    path('order/<int:pk>/', AdminOrderDetailView.as_view(), name='order_detail'),
-    path('order/<int:pk>/delete/', AdminDeleteOrder.as_view(), name='delete_order'),
+    path('order/<int:pk>/', AdminOrderDetailView.as_view(),
+         name='order_detail'),
+    path('order/<int:pk>/delete/', AdminDeleteOrder.as_view(),
+         name='delete_order'),
 ]
